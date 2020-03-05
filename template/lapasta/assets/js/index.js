8face767eaffb4a301782c03168fef3e2e8dec3f
william175 <bill5986@gmail.com>
2020-03-05 18:24:53 +0800

    add 蓋板廣告程式 index.js

--- template/lapasta/assets/js/index.js
+++ template/lapasta/assets/js/index.js
@@ -1,1 +1,12 @@
+// 蓋板廣告
 
+let adOutBox = document.querySelector('#ad-content');
+let ad = document.querySelector('#ad');
+
+window.onload = function () {
+    ad.style.display = " block";
+}
+function removeAD() {
+    adOutBox.removeChild(ad);
+}
+setTimeout(removeAD, 1000);
