7cb58de6636e19d51bc5ac504c6bbd7709f0ad2a
william175 <bill5986@gmail.com>
2020-03-14 23:29:55 +0800

    新增首頁輪播

--- template/lapasta/assets/js/index.js
+++ template/lapasta/assets/js/index.js
@@ -10,3 +10,25 @@ function removeAD() {
     adOutBox.removeChild(ad);
 }
 setTimeout(removeAD, 1000);
+
+// 簡易輪播
+var poster = document.querySelector('#poster');
+var pic = 1;
+var timer;
+function carousel() {
+    if (pic < 4) {
+        pic++;
+    } else {
+        pic = 1;
+    }
+    poster.src = "./assets/img/pasta/pasta0" + pic + ".jpg";
+}
+timer = setInterval(carousel, 2000);
+
+// function docarousel() {
+//     clearInterval(timer);
+//     timer = setInterval(carousel, 1000); //
+// }
+// function stopcarousel() {
+//     clearInterval(timer);
+// }
