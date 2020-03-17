2101a3c0bcf084521e53305bb7f69c78adad81cf
william175 <bill5986@gmail.com>
2020-03-17 22:32:46 +0800

    修改js檔案

--- template/lapasta/assets/js/index.js
+++ template/lapasta/assets/js/index.js
@@ -1,5 +1,4 @@
 // 蓋板廣告
-
 let adOutBox = document.querySelector('#ad-content');
 let ad = document.querySelector('#ad');
 
@@ -9,7 +8,25 @@ window.onload = function () {
 function removeAD() {
     adOutBox.removeChild(ad);
 }
-setTimeout(removeAD, 1000);
+setTimeout(removeAD, 50000);
+// 蓋板廣告BTN關閉紐
+document.querySelector('#ad-btn').addEventListener('click', function () {
+    ad.style.display = " none";
+})
+
+
+//登入框 與內建關閉按鈕
+document.querySelector('#SignInBtn').addEventListener('click', function () {
+    document.querySelector('#singin-box').style.display = "block";
+});
+
+document.querySelector('#close').addEventListener('click', function () {
+    document.querySelector('#singin-box').style.display = " none";
+})
+
+
+
+
 
 // 簡易輪播
 var poster = document.querySelector('#poster');
@@ -24,11 +41,3 @@ function carousel() {
     poster.src = "./assets/img/pasta/pasta0" + pic + ".jpg";
 }
 timer = setInterval(carousel, 2000);
-
-// function docarousel() {
-//     clearInterval(timer);
-//     timer = setInterval(carousel, 1000); //
-// }
-// function stopcarousel() {
-//     clearInterval(timer);
-// }
