da2567a39cafe8104cd182d14d77ca6cff333318
william175 <bill5986@gmail.com>
2020-03-20 21:00:12 +0800

    修改旅遊網 footer 高度

---
+++ react-practice/travel-page/src/components/footerStyle.js
@@ -0,0 +1,14 @@
+const footerStyle = {
+  footer: {
+    display: "fixed",
+    bottom: "0",
+    width: "100%",
+    lineHeight: "100px",
+    height: "100px",
+    backgroundColor: "#009688",
+    color: "#fff",
+    marginBottom: "0"
+  }
+};
+
+export default footerStyle;
