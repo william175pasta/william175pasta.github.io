8f336a33e71a0982d25f7b9e712a8792093ba251
william175 <bill5986@gmail.com>
2020-03-26 23:44:39 +0800

    no message

--- resume/src/components/Header/NavBarLinks.jsx
+++ resume/src/components/Header/NavBarLinks.jsx
@@ -11,29 +11,29 @@ const useStyles = makeStyles(styles);
 
 /**
  * 導覽列連結
- * @param {*} props 
+ * @param {*} props
  */
-const NavBarLinks = (props) => {
-	const classes = useStyles();
-	return (
-		<List className={classes.list}>
-			<ListItem className={classes.listItem}>
-				<Link to="/about" className={classes.dropdownLink}>
-					關於我
-				</Link>
-			</ListItem>
-			<ListItem className={classes.listItem}>
-				<Link to="/works" className={classes.dropdownLink}>
-					工作經歷
-				</Link>
-			</ListItem>
-			<ListItem className={classes.listItem}>
-				<Link to="/mainDish" className={classes.dropdownLink}>
-					私房料理
-				</Link>
-			</ListItem>
-		</List>
-	);
+const NavBarLinks = props => {
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <Link to="/about" className={classes.dropdownLink}>
+          關於我
+        </Link>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Link to="/works" className={classes.dropdownLink}>
+          工作經歷
+        </Link>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Link to="/mainDish" className={classes.dropdownLink}>
+          私房料理
+        </Link>
+      </ListItem>
+    </List>
+  );
 };
 
 export default NavBarLinks;
--- resume/src/components/Header/index.jsx
+++ resume/src/components/Header/index.jsx
@@ -12,57 +12,56 @@ const useStyles = makeStyles(styles);
 
 /**
  * 頁面標頭元件
- * @param {*} props 
+ * @param {*} props
  */
-const Header = (props) => {
-	const classes = useStyles();
-	useEffect(() => {
-		if (props.changeColorOnScroll) {
-			window.addEventListener("scroll", headerColorChange);
-		}
-		return function cleanup() {
-			if (props.changeColorOnScroll) {
-				window.removeEventListener("scroll", headerColorChange);
-			}
-		};
-	});
-	const headerColorChange = () => {
-		const { color, changeColorOnScroll } = props;
-		const windowsScrollTop = window.pageYOffset;
-		if (windowsScrollTop > changeColorOnScroll.height) {
-			document.body
-				.getElementsByTagName("header")[0]
-				.classList.remove(classes[color]);
-			document.body
-				.getElementsByTagName("header")[0]
-				.classList.add(classes[changeColorOnScroll.color]);
-		} else {
-			document.body
-				.getElementsByTagName("header")[0]
-				.classList.add(classes[color]);
-			document.body
-				.getElementsByTagName("header")[0]
-				.classList.remove(classes[changeColorOnScroll.color]);
-		}
-	};
-	const { color, rightLinks, leftLinks, brand, fixed, absolute } = props;
-	const appBarClasses = classNames({
-		[classes.appBar]: true,
-		[classes[color]]: color,
-		[classes.absolute]: absolute,
-		[classes.fixed]: fixed
-	});
+const Header = props => {
+  const classes = useStyles();
+  useEffect(() => {
+    if (props.changeColorOnScroll) {
+      window.addEventListener("scroll", headerColorChange);
+    }
+    return function cleanup() {
+      if (props.changeColorOnScroll) {
+        window.removeEventListener("scroll", headerColorChange);
+      }
+    };
+  });
+  const headerColorChange = () => {
+    const { color, changeColorOnScroll } = props;
+    const windowsScrollTop = window.pageYOffset;
+    if (windowsScrollTop > changeColorOnScroll.height) {
+      document.body
+        .getElementsByTagName("header")[0]
+        .classList.remove(classes[color]);
+      document.body
+        .getElementsByTagName("header")[0]
+        .classList.add(classes[changeColorOnScroll.color]);
+    } else {
+      document.body
+        .getElementsByTagName("header")[0]
+        .classList.add(classes[color]);
+      document.body
+        .getElementsByTagName("header")[0]
+        .classList.remove(classes[changeColorOnScroll.color]);
+    }
+  };
+  const { color, rightLinks, leftLinks, brand, fixed, absolute } = props;
+  const appBarClasses = classNames({
+    [classes.appBar]: true,
+    [classes[color]]: color,
+    [classes.absolute]: absolute,
+    [classes.fixed]: fixed
+  });
 
-	return (
-		<AppBar className={appBarClasses}>
-			<Toolbar className={classes.container}>
-				<a className={classes.title} href="/">william</a>
-				<Hidden smDown implementation="css">
-					<NavBarLinks />
-				</Hidden>
-			</Toolbar>
-		</AppBar>
-	);
+  return (
+    <AppBar className={appBarClasses}>
+      <Toolbar className={classes.container}>
+        {/* <Hidden smDown implementation="css"> */}
+          <NavBarLinks />
+        {/* </Hidden> */}
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
