e6105f779d5a35e952826dbdcd6135b1166d1c11
william175 <bill5986@gmail.com>
2020-03-26 23:43:10 +0800

    add  個人介紹元件css樣式

---
+++ resume/src/views/AboutPage/aboutmePage.js
@@ -0,0 +1,27 @@
+const aboutmeStyle = {
+  background: {
+    background: "#252b42",
+    color: "#eee"
+  },
+  aboutmeContent: {
+    margin: "50px 10px 0"
+  },
+  title: {
+    textAlign: "center",
+    fontSize: "26px"
+  },
+  text: {
+    fontSize: "18px",
+    textAlign: "justify",
+    padding: "10px",
+    borderBottom: "1px dotted black",
+    textAlign: "center"
+  },
+  contacttext: {
+    fontSize: "18px",
+    textAlign: "justify",
+    padding: "10px",
+    textAlign: "end"
+  }
+};
+export default aboutmeStyle;
