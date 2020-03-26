218c7ef8d7edbe9456c0b934ead51969a5edbe17
william175 <bill5986@gmail.com>
2020-03-26 23:45:33 +0800

    no message

--- resume/src/components/Parallax/parallaxStyle.js
+++ resume/src/components/Parallax/parallaxStyle.js
@@ -1,10 +1,10 @@
 const parallaxStyle = {
   parallax: {
     height: "90vh",
-    maxHeight: "1000px",
-    overflow: "hidden",
+    maxHeight: "1000px  ",
+    overflow: "hidden  ",
     position: "relative",
-    backgroundPosition: "center center",
+    backgroundPosition: "center center  ",
     backgroundSize: "cover",
     margin: "0",
     padding: "0",
--- resume/src/views/HomePage/sectionStyle.js
+++ resume/src/views/HomePage/sectionStyle.js
@@ -1,26 +1,26 @@
-import { title } from "../../assets/js/material-kit-react.js";
+import { title } from '../../assets/js/material-kit-react.js';
 
 const sectionStyle = {
-  section: {
-    padding: "70px 0",
-    textAlign: "center"
-  },
-  title: {
-    ...title,
-    marginBottom: "1rem",
-    marginTop: "30px",
-    minHeight: "32px",
-    textDecoration: "none"
-  },
-  description: {
-    color: "#999"
-  },
-  roundImg: {
-    padding: "0px",
-    borderRadius: "100px",
-    cursor: "pointer",
-    boxShadow: "0 0 10px #34465d"
-  }
+    section: {
+        padding: '70px 0',
+        textAlign: 'center',
+    },
+    title: {
+        ...title,
+        marginBottom: '1rem',
+        marginTop: '30px',
+        minHeight: '32px',
+        textDecoration: 'none',
+    },
+    description: {
+        color: '#999',
+    },
+    roundImg: {
+        padding: '0px',
+        borderRadius: '100px',
+        cursor: 'pointer',
+        boxShadow: '0 0 10px #34465d',
+    },
 };
 
 export default sectionStyle;
